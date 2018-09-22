20363622f47e41fb1ea2dbee14157372f313fea7
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-22 21:23:34 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -5,6 +5,8 @@ class SchoolClass < ApplicationController
 
     def create
       @school_class = SchoolClass.new(params.require(:school_class))
+      @school_class.save
+      redirect_to school_class_path(@school_class)
     end
 
     def show
