00e71b2a04fc628a44b60a7e07b074baa738bee4
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-22 21:25:25 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -10,14 +10,16 @@ class SchoolClass < ApplicationController
     end
 
     def show
-
+      @school_class = SchoolClass.find(params[:id])
     end
 
     def edit
-
+      @school_class = SchoolClass.find(params[:id])
     end
 
     def update
-
+      @school_class = SchoolClass.find(params[:id])
+      @school_class.update(params.require(:school_class))
+      redirect_to school_class_path(@shcool_class)
     end
 end
