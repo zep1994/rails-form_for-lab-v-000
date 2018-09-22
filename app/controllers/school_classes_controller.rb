fd40230a872d1b9b99ac87241164810e0ce2b9a2
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-22 22:05:08 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -3,6 +3,7 @@ class SchoolClassesController < ApplicationController
     def index
       @school_class = SchoolClass.all
     end
+    
     def new
       @school_class = SchoolClass.new
     end
